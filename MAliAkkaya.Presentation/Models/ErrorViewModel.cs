05c3c4dd8ea47f834aa3c4c16061f3f559137d39
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2022-08-03 23:14:04 +0300

    Add project files.

---
+++ MAliAkkaya.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MAliAkkaya.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
